34401386c5bb1924732b699472d480045369ce3a
minh1 <2kchanh@gmail.com>
2024-01-02 18:49:14 +0700

    new project

---
+++ src/main/java/com/chanhmn/backendforum/BackendForumApplication.java
@@ -0,0 +1,13 @@
+package com.chanhmn.backendforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chanhmn/backendforum/BackendForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanhmn.backendforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
